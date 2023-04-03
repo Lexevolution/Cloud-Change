7e2d0ee5860201eee67c56e300efb87d71464b7f
Lexevolution <31176843+Lexevolution@users.noreply.github.com>
2023-04-03 21:17:53 +1000

    Made the mod :)

--- CloudChange/CloudChange.cs
+++ CloudChange/CloudChange.cs
@@ -4,10 +4,10 @@ using FrooxEngine;
 using HarmonyLib;
 using NeosModLoader;
 using UnityEngine;
+using CloudX.Shared;
 
 namespace CloudChange
 {
-	[HarmonyPatch]
 	class CloudChange : NeosMod
 	{
 		public override string Name => BuildInfo.Name;
@@ -15,8 +15,25 @@ namespace CloudChange
 		public override string Version => BuildInfo.Version;
 		public override string Link => BuildInfo.Link;
 
-		public override void OnEngineInit()
+		[AutoRegisterConfigKey]
+		private static readonly ModConfigurationKey<string> API_URL = new("API URL", "API URL: The endpoint for most of the API actions in Neos.", () => "https://cloudx.azurewebsites.net");
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> BLOB_STORAGE_URL = new("Blob Storage URL", "Blob Storage URL: The root endpoint which stores all the assets for all objects.", () => "https://cloudxstorage.blob.core.windows.net/");
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> ASSETS_BLOB_URL = new("Blob Retrieval URL", "Blob Retrieval URL: The endpoint which retrieves the assets stored at the Blob Storage URL", () => "https://cloudxstorage.blob.core.windows.net/assets/");
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> ASSETS_CDN_URL = new("CDN URL", "CDN URL: The endpoint which delivers assets faster than the Blob Retrieval URL.", () => "https://cloudx.azureedge.net/assets/");
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> VIDEO_CDN_URL = new("Video CDN URL", "Video CDN URL: The endpoint which delivers specifically video assets saved to your inventory (neodb videos)", () => "https://cloudx.azureedge.net/assets/");
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> OLD_THUMBNAIL_URL = new("Old Thumbnail URL", "Old Thumbnail URL: The endpoint which used to store and retrieve thumbnails for sessions.", () => "https://cloudxstorage.blob.core.windows.net/thumbnails/");
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> THUMBNAIL_URL = new("Thumbnail URL", "Thumbnail URL: The endpoint which stores and retrieves thumbnails for sessions.", () => "https://cloudxoperationalblob.blob.core.windows.net/thumbnails/");
+
+		private static ModConfiguration config;
+        public override void OnEngineInit()
 		{
+			config = GetConfiguration();
 			try
 			{
 				Harmony harmony = new(BuildInfo.GUID);
@@ -28,6 +45,58 @@ namespace CloudChange
 			}
 		}
 
+        [HarmonyPatch(typeof(CloudXInterface))]
+        class URL_Patches
+		{
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_API", MethodType.Getter)]
+            public static void ChangeAPI_URL(ref string __result)
+            {
+				__result = config.GetValue(API_URL);
+			}
 
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_BLOB", MethodType.Getter)]
+            public static void ChangeBlobURL(ref string __result)
+            {
+                __result = config.GetValue(BLOB_STORAGE_URL);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_ASSETS_BLOB", MethodType.Getter)]
+            public static void ChangeAssetsBlobURL(ref string __result)
+            {
+                __result = config.GetValue(ASSETS_BLOB_URL);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_ASSETS_CDN", MethodType.Getter)]
+            public static void ChangeCDN_URL(ref string __result)
+            {
+                __result = config.GetValue(ASSETS_CDN_URL);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_ASSETS_VIDEO_CDN", MethodType.Getter)]
+            public static void ChangeVideoCDN_URL(ref string __result)
+            {
+                __result = config.GetValue(VIDEO_CDN_URL);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_THUMBNAILS_OLD", MethodType.Getter)]
+            public static void ChangeOldThumbnailURL(ref string __result)
+            {
+                __result = config.GetValue(OLD_THUMBNAIL_URL);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch("NEOS_THUMBNAILS", MethodType.Getter)]
+            public static void ChangeThumbnailURL(ref string __result)
+            {
+                __result = config.GetValue(THUMBNAIL_URL);
+            }
+        }
+		
 	}
 }
--- README.md
+++ README.md
@@ -6,7 +6,13 @@ The current default options also fix some of the issues [here](https://github.co
 
 ## Config Options
 
-TODO
+- API URL (Default `https://cloudx.azurewebsites.net`): The endpoint for most of the API actions in Neos. Changing this will change where most cloud actions go to and from. You would likely need to host your own Neos Cloud to change this.
+- Blob Storage URL (Default `https://cloudxstorage.blob.core.windows.net/`): The root endpoint which stores all the assets for all objects. Changing this would change which blob storage thyour neosdb assets go to. You would likely need to host your own blob storage server to change this.
+- Blob Retrieval URL (Default `https://cloudxstorage.blob.core.windows.net/assets/`): The endpoint which retrieves the assets stored at the Blob Storage URL.
+- CDN URL (Default `https://cloudx.azureedge.net/assets/`): The Content Delivery Network URL that is used to deliver assets faster than the Blob Retrieval URL. You would likely need to proxy the asset server through a CDN to change this.
+- Video CDN URL (Neos Default `https://cloudx-video.azureedge.net/`, Mod Default `https://cloudx.azureedge.net/assets/`): The endpoint which delivers specifically video assets saved to your inventory (neodb videos). This mod's default "fixes" part of the issue mentioned above.
+- Old Thumbnail URL (Default `https://cloudxstorage.blob.core.windows.net/thumbnails/`): The endpoint which used to store and retrieve thumbnails for sessions. This likely isn't used anymore and doesn't need to be changed. Added it in just in case.
+- Thumbnail URL (Defualt `https://cloudxoperationalblob.blob.core.windows.net/thumbnails/`): The endpoint which stores and retrieves thumbnails for sessions. This mod adds a `/` at the end to fix part of the issue mentioned above.
 
 ## Building
 
